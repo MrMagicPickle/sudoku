d0b59472d6ff079ca71a409744f54299a453dfb6
Wei Shen Tan <tanweishen97@gmail.com>
2024-09-17 22:02:23 +0800

    WIP: Render grid

--- app/src/Room.tsx
+++ app/src/Room.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { APP_ID, Schema } from "./db";
 import { useEffect } from "react";
 import { init } from "@instantdb/react";
+import './Sudoku.css';
 
 // const db = getDb();
 const db = init<Schema>({ appId: APP_ID });
@@ -20,16 +21,53 @@ function Room() {
   });
 
   console.log(data, '<< data');
-  // useEffect(() => {
-  //   console.log(data, '<< data');
-  // }, [data]);
-  // useEffect(() => {
 
-  // }, []);
+  const renderGrid = () => {
+    if (!data) {
+      return;
+    }
+
+    const { sudokuRoom } = data;
+    const { sudokuGameState } = sudokuRoom[0];
+    const { boardState } = sudokuGameState[0];
+
+
+    /* Convert to quadrants */
+    const quadrantsToCells: Record<string, (number | null)[][]> = {};
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        // Ranges from [0..2]
+        const quadRow = Math.floor(i / 3);
+        const quadCol = Math.floor(j / 3);
+        const cellsInQuad = quadrantsToCells[`${quadRow}-${quadCol}`];
+
+        const cellNumber = boardState[`${i},${j}`];
+        const cellX = i;
+        const cellY = j;
+        quadrantsToCells[`${quadRow}-${quadCol}`] = cellsInQuad ?
+          [...cellsInQuad, [cellNumber, cellX, cellY]] :
+          [[cellNumber, cellX, cellY]];
+      }
+    }
+
+    const divQuads = Object.entries(quadrantsToCells).map(([key, value], index) => {
+      return <div className="quadrant" key={`quad-${index}`}>
+        {
+          value.map((cell, index) => {
+            return <div id={`cell-${cell[1]}-${cell[2]}`} className="cell" key={`cell-${index}`}>{ cell[0] }</div>
+          })
+        }
+      </div>
+    });
+    return divQuads;
+  }
 
 
   return (<>
     <p> Room: { roomId } </p>
+    <div className='grid-container'>
+      { renderGrid() }
+    </div>
   </>)
 }
 
